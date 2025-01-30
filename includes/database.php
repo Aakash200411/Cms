ff37153558868f91530e95e0cffba16fa8a4159c
Aakash200411 <146609865+Aakash200411@users.noreply.github.com>
2025-01-30 17:59:52 +0530

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect('mysql.railway.app', 'root', 'YTHbOgINtFZqRZcABqbrRxiPOtnJcqQE', 'railway');
+$connect = mysqli_connect('mysql.railway.internal', 'root', 'YTHbOgINtFZqRZcABqbrRxiPOtnJcqQE', 'railway');
 
 if (mysqli_connect_errno()) {
     exit('Failed to connect to Mysql : ' . mysqli_connect_error());
