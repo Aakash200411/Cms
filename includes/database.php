4987b7ff86ee6a2b996c9d6105ee77ce1a7013e5
Aakash200411 <146609865+Aakash200411@users.noreply.github.com>
2025-02-01 19:58:49 +0530

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect('mysql.railway.app', 'root', 'fQztJpobwvoAeTvYascEhKjpAONNkJQG', 'railway');
+$connect = mysqli_connect('mysql.db.mdbgo.com', 'aakash200411_cmsdb', 'Secret@cms1', 'aakash200411_cmsdb');
 
 if (mysqli_connect_errno()) {
     exit('Failed to connect to Mysql : ' . mysqli_connect_error());
