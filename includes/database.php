09733177188bd84539806c03f78a7600611f7d74
Aakash200411 <146609865+Aakash200411@users.noreply.github.com>
2025-02-03 17:41:28 +0530

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect('mysql.db.mdbgo.com', 'aakash200411_cmsdb', 'Secret@cms1', 'aakash200411_cmsdb');
+$connect = mysqli_connect('localhost', 'cms', 'secret@cms', 'cms');
 
 if (mysqli_connect_errno()) {
     exit('Failed to connect to Mysql : ' . mysqli_connect_error());
